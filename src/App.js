27231409aa0429bed431c512944942495c2a69b5
Pradeep <mathrupradeep@gmail.com>
2019-05-06 11:04:36 +0530

    adding http and log service

--- src/App.js
+++ src/App.js
@@ -2,32 +2,37 @@ import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
 import NotFound from "./components/notfound";
+import { ToastContainer } from "react-toastify";
 import NavBar from "./components/navbar";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <NavBar />
-        <Switch>
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/login" component={LoginForm} />
-          <Route path="/movies/new" component={MovieForm} />
-          <Route path="/movies/:id" component={MovieForm} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <ToastContainer />
+        <main className="container">
+          <NavBar />
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/new" component={MovieForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
